f37115b09690d378e39c1a60604e01e5a9e7128a
Bryce Hasshaw <bhasshaw@gmail.com>
2018-08-29 19:33:17 -0700

    added mongo

--- server.js
+++ server.js
@@ -14,10 +14,8 @@ const { PORT, DATABASE_URL } = require('./config');
 
 const app = express();
 
-app.use(express.static('public'));
-
+app.use(express.static('../public'));
 app.use(morgan('common'));
-
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
