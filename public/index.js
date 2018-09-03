d96bbae686b9bbc1948b0b08e03dc3a46072853f
Bryce Hasshaw <bhasshaw@gmail.com>
2018-09-03 13:52:50 -0700

    working on login and signup

--- public/index.js
+++ public/index.js
@@ -1,1 +1,29 @@
 'use strict';
+
+// LOGIN
+
+function logInListener () {
+    $('#login-submit-btn').click(function(event) {
+        event.preventDefault();
+        logIn();
+    });
+};
+
+function logIn () {
+    let username = $('#login-username').val();
+    let password = $('#login-password').val();
+    logInRequest(username, password);
+};
+
+function logInRequest (username, password) {
+    let logInInfo = {
+        username,
+        password
+    };
+    $.ajax({
+        url: '/api/auth/login',
+        type: 'POST',
+        data: JSON.stringify(logInInfo),
+        contentType: 'application/json'
+    })
+};
--- public/signup.js
+++ public/signup.js
@@ -1,1 +1,26 @@
 'use strict';
+
+// SIGNUP
+
+function signupListener () {
+    $('#signup-submit-btn').click(function(event) {
+        event.preventDefault();
+        signup();
+    });
+};
+
+function signup() {
+    let signupInfo = {
+        username = $('#signup-username').val(),
+        password = $('#signup-password').val(),
+        year = $('#signup-year').val(),
+        make = $('#signup-make').val(),
+        model = $('#signup-model').val()
+    }
+    $.ajax({
+        url: '/api/users',
+        type: 'POST',
+        data: JSON.stringify(signupInfo),
+        contentType: 'application/json'
+    })
+};
