591feacd6348a78d4a7af0ae980f954be87a72d5
Bryce Hasshaw <bhasshaw@gmail.com>
2018-09-20 20:59:59 -0700

    created test endpoints

--- test/test-service.js
+++ test/test-service.js
@@ -4,45 +4,101 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
+
 const expect = chai.expect;
 
 const { app, runServer, closeServer } = require('../server');
-const { Service } = require('../service/models');
-const { User } = require('../users/models');
+const { Service } = require('../service/router');
 const { JWT_SECRET, DATABASE_URL } = require('../config');
 
 chai.use(chaiHttp);
 
-let token;
-
-// function cleanDB() {
-//     return new Promise((resolve, reject) => {
-//       mongoose.connection.dropDatabase()
-//         .then((result) => {
-//           resolve(result);
-//         })
-//         .catch((error) => {
-//           reject(error);
-//         });
-//     });
-// };
+const token = jwt.sign(
+    {
+        user :{
+            username: "user4"
+        }
+    },
+    JWT_SECRET,
+    {
+        algorithm: 'HS256',
+        expiresIn: '7d'
+    }
+);
 
 describe('Service', function () {
     before(() => {
-        runServer(DATABASE_URL);
+        return runServer(DATABASE_URL);
     });
     after(() => {
-        // cleanDB();
-        closeServer();
+        return closeServer();
     });
 
     it('should list items on GET', function () {
         return chai
             .request(app)
-            .get('/posts/:username')
-            .set('authorization', `Bearer ${token}`)
+            .get('/api/service/posts/testuser')
+            .set('Authorization', `Bearer ${token}`)
             .then(function(res) {
                 expect(res).to.have.status(200)
             })    
     });
+
+    it('should add items on POST', function () {
+        const newPost = {
+            username: "testuser",
+            description: "Oil Change",
+            date: "09/18/2018",
+            miles: "15000",
+            cost: "50"
+        }
+
+        return chai
+            .request(app)
+            .post('/api/service/posts')
+            .set('Authorization', `Bearer ${token}`)
+            .send(newPost)
+            .then(function(res) {
+                expect(res).to.have.status(201)
+            })    
+    });
+
+    it('should delete items on DELETE', function () {
+        return chai
+            .request(app)
+            .get('/api/service/posts/testuser')
+            .set('Authorization', `Bearer ${token}`)
+            .then(function(res) {
+                return chai.request(app)
+                    .delete(`/api/service/posts/${res.body[0].id}`)
+                    .set('Authorization', `Bearer ${token}`)
+            }) 
+            .then(function(res) {
+                expect(res).to.have.status(204)
+            });   
+    });
+
+    it('should update items on PUT', function () {
+        const updateData = {
+            username: "testuser",
+            description: "Oil Change",
+            date: "09/10/2018",
+            miles: "15000",
+            cost: "60"
+        }
+        return chai
+            .request(app)
+            .get('/api/service/posts/testuser')
+            .set('Authorization', `Bearer ${token}`)
+            .then(function(res) {
+                return chai.request(app)
+                    .put(`/api/service/posts/${res.body[0].id}`)
+                    .set('Authorization', `Bearer ${token}`)
+                    .send(updateData)
+            }) 
+            .then(function(res) {
+                expect(res).to.have.status(204)
+            });   
+    });
+
 });
