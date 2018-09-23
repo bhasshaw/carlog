c1d00c2ab3b3e14e782e860e6ca90da9e24cf115
Bryce Hasshaw <bhasshaw@gmail.com>
2018-09-23 10:27:08 -0700

    linking mlab

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/carlog';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://newuser:newuser1@ds161913.mlab.com:61913/carlog';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-carlog';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
